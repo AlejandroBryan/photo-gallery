404b39debdb9626d3ee55647050613c9cdf8d415
Alejandro Bryan <alejandrobryan023@gmail.com>
2020-02-03 23:42:21 +0100

    new file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
   lightbox.option({
       'resizeDuration': 200,
       'wrapAround': true,
-      "showImageNumberLabel" : true
-      
+      "showImageNumberLabel" : true,
+      alwaysShowNavOnTouchDevices: true
     });
+
+
+const searchBar = $('#myInput');
+
+searchBar.on('keyup', function(e) {
+    
+    // Grab the search term and convert to lower case.
+    const term = e.target.value.toLowerCase();
+    
+    // Grab all of the captions, cycle through each one and log.
+    $('.mygallery a').each(function () {
+        const caption = $(this).attr("data-title").toLowerCase();
+        
+        
+        // If search term is in the caption, show the image/s associated to the caption.
+        if (caption.toLowerCase().indexOf(term) != -1) {
+            $(this).show();
+            
+        } // Otherwise, hide the image/s if the search term is not in the caption. 
+        else {
+            $(this).hide();
+        }
+        
+    });
+    
+}); 
